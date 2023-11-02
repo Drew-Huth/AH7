5fb721e350929a8a656eedd12634acce1e612976
Drew-Huth <drew.huth@fiserv.com>
2023-11-02 13:35:49 -0400

    please

---
+++ Source.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Node<T> {
+  T val;
+  Node<T> next;
+
+  public Node(T val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Source {
+  public static int sumList(Node<Integer> head) {
+    List<Integer> nodeList = new ArrayList<>();
+    int sum = 0;
+    while (head != null) {
+      nodeList.add(head.val);
+      head = head.next;
+    }
+    for (int i = 0; i < nodeList.size(); i++) {
+      sum += nodeList.get(i);
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    Node<Integer> a = new Node<>(2);
+    Node<Integer> b = new Node<>(8);
+    Node<Integer> c = new Node<>(3);
+    Node<Integer> d = new Node<>(-1);
+    Node<Integer> e = new Node<>(7);
+
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    d.next = e;
+
+    // 2 -> 8 -> 3 -> -1 -> 7
+
+    System.out.println("Sum of my list: ");
+    System.out.println(Source.sumList(a));
+  }
+}
